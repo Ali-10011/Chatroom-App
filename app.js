a4a91c2f520f5ff8b0013720bc79ec7090e01b67
Ali-10011 <98711622+Ali-10011@users.noreply.github.com>
2022-08-27 01:37:06 +0500

    errmsgs implemented, sockets.

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const mongoose = require('mongoose');
 const { isNull } = require('util');
 const ChatData = require('./models/ChatData');
 var SessionUser;
+var errmsg;
 const app = express();
-
+var http = require('http').createServer(app);
+var io = require("socket.io")(http);
+const {joinUser, removeUser} = require('./models/UsersData');
 app.set('view engine', 'ejs'); //ejs looks for view files in a "view" folder, so you have to have one made
 app.use(express.urlencoded({ extended: true })); //some sort of encoding, to make sure data doesn't get crappy
 
 const uri = 'mongodb+srv://Art:Art0309@node-practice.jknzmex.mongodb.net/Chat-App'; //name of the database we want to access, otherwise it will assign it to a database named "test"
-mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true }).then((data)=>{console.log('Connected');app.listen(3000);}).catch((err)=>{console.log(err)});
+mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then((data)=>{ 
+io.on("connection", function (socket){
+  console.log("socket connected");
+  socket.on("join room", (data)=>{
+   let UserInstance = joinUser(socket.id, data.username, data.roomname);
+   socket.emit('send data', UserInstance);
+  
+  }) } ); 
+  http.listen(3000);})
+.catch((err)=>{console.log(err)});
  //we only want to listen to request when the database has been connected
  //listen from port number 3000, it automatically knows it's localhost
 app.get('/', (req, res) => {
@@ -19,7 +32,7 @@ app.get('/', (req, res) => {
   });
   //on one page, you can either one of each type request
   app.get('/login', (req, res) => {
-    res.render('login', { title: 'Auth' }); //it will render login.ejs file
+    res.render('login', { title: 'Auth', errmsg: errmsg }); //it will render login.ejs file
   }); //we can send variable that ejs will render
   
   app.post('/login', (req, res) => {
@@ -33,6 +46,7 @@ app.get('/', (req, res) => {
             if(person)
             {
            console.log("This Person Already exists");
+           errmsg = 'This Person Already exists';
            return res.redirect('/login');           
             }
             else 
@@ -54,7 +68,7 @@ else
   {
     ChatData.findOne({ 'username': req.body.username }, function (err, person) { //returns a callback function
         if (err) return handleError(err);
-        // Prints "Space Ghost is a talk show host".
+        
         if(person)
         {
             if(person.password == Chat.password)
@@ -66,12 +80,14 @@ else
             else 
             {
                 console.log("Wrong Password");
+                errmsg = 'Wrong Password';
                 return res.redirect('/login');
             }       
         }
         else 
         {
-       console.log("This user does not exist, Perhaps Try signing in");
+       console.log("This user does not exist, Perhaps Try signing up");
+       errmsg = 'This user does not exist, Perhaps Try signing up';
        return res.redirect('/login');
             }
       });
